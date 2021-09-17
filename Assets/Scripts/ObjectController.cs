56cc07ef85b82ece475874575fae88c5474f0228
Arecanon <arecanon@gmail.com>
2021-09-17 20:41:04 +0900

    Add shader&resource / objectController fix

--- Assets/Scripts/ObjectController.cs
+++ Assets/Scripts/ObjectController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3c5657ad7f68c79abb26d66c9d60b1afbcb7b53170dd519b2c91d137ac3e9f41
-size 2212
+oid sha256:1e5229af2fb9d3d747f2330c7483be49a969256a7937bdda0bd8f6d6eae6c6bd
+size 2649
