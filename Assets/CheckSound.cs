b5786832b49712a2c58ce1912404decb8dcc708c
amkorousagi <hasmi5452@gmail.com>
2021-10-04 19:09:47 +0900

    found asset

---
+++ Assets/CheckSound.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:24741e40fed8c4891d103e2744403210776876eee133e852f6970896a25c1b86
+size 351
---
+++ Assets/Scripts/SoundController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a79d09dd47d6c39e236a395173d3ac09a934fb4c1f1aa89c8d8af91e457da355
+size 449
