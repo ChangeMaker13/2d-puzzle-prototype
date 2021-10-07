1e559e258f77389313e8850f403a26e5457a59c5
ChangeMaker13 <wns7756@naver.com>
2021-10-07 12:01:20 +0900

    bug fix : main game node not moving

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f30ac084f3d25a980bd2e8bd9e3a114dd0e6d5125fb9a8972103d4982c87507d
-size 1169
+oid sha256:e730097d7fa72b83ef1d8b3ccd66538285c5a4be3d4a8ac7928719163aa8eef8
+size 1221
