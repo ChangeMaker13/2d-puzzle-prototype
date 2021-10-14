2fb25a4f8d082895cb0233a2c379a17939d68927
amkorousagi <hasmi5452@gmail.com>
2021-10-14 17:29:43 +0900

    feat: init bike btn

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e730097d7fa72b83ef1d8b3ccd66538285c5a4be3d4a8ac7928719163aa8eef8
-size 1221
+oid sha256:747c09c1fe5f0252d16e4f2682e8d1e6a310cd0fa8dca2b7d0fc14c5582016d8
+size 1615
