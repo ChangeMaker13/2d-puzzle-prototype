4c7f81c1ec81a97083e35d66181c8a9066155b55
amkorousagi <hasmi5452@gmail.com>
2021-10-15 17:30:33 +0900

    feat : after stage, playerpref coin

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:747c09c1fe5f0252d16e4f2682e8d1e6a310cd0fa8dca2b7d0fc14c5582016d8
-size 1615
+oid sha256:3a78448a5546e4a2292c5cc278ad18090322ebde2a968cf4c7c56e0708117c4c
+size 1638
---
+++ Assets/Scripts/UI&Graphics/GoalCollider.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c1713f1791509986c6358cfafb48bc4ea20cf49979edbd7f6ceecc7b950205ad
+size 1337
