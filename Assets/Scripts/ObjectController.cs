bb0108402d48134e471a9b678f79b047714b6de4
amkorousagi <hasmi5452@gmail.com>
2021-11-01 19:11:46 +0900

    feat : backgorund controller for change background, SetSelectedNodesEffect for InitBnt and GoBtn

--- Assets/Scripts/ObjectController.cs
+++ Assets/Scripts/ObjectController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f63a7174c7e99a5ef4c503ed9b822942c6bdc82dbe8ed8d3ba4853e0ffd4c39a
-size 2896
+oid sha256:ebc2d67f11544f81fc6776231ac16a5f59da73ac71e89652999860a048c16869
+size 3271
