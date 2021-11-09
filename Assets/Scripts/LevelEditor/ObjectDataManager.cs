6a15a97ed12ebe955ce128e0de0c79169886dee4
Lemiraum <hope9667@gmail.com>
2021-11-09 20:44:32 +0900

    device update

--- Assets/Scripts/LevelEditor/ObjectDataManager.cs
+++ Assets/Scripts/LevelEditor/ObjectDataManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be654141365490a127ae3b3051296ea650c0ea29c8a52c247797140b3d589412
-size 8372
+oid sha256:ae7300f0c9c540957762c0e2faa477ff7be85c4286bc7ae72d41599b9ece2d48
+size 8389
