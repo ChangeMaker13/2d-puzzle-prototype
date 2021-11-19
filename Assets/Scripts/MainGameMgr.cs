1098ef43558e86d493a909662729b7e0e6490072
amkorousagi <hasmi5452@gmail.com>
2021-11-20 05:05:48 +0900

    feat : shop

--- Assets/Scripts/MainGameMgr.cs
+++ Assets/Scripts/MainGameMgr.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:35da147d6e15baf545dc4510956fb011cce874a6129cded6ec050319fab6c1b6
-size 1613
+oid sha256:bcbe29cb2166da2d4dae63abb4d30a41efbbd3975e686a2e1e6df299ae425a97
+size 1795
---
+++ Assets/shopCoinText.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2c54caa666656d70da8914473690d60ad79e4f47a7cbdb179f5172e1c49ef474
+size 460
