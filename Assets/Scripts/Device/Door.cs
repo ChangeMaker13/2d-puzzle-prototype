db300fb55223c16bb1acee0c0ef9b232b27947d4
ChangeMaker13 <wns7756@naver.com>
2021-11-22 17:10:45 +0900

    state of the sunday meeting(11/22)

--- Assets/Scripts/Device/Door.cs
+++ Assets/Scripts/Device/Door.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5425b8e091582b3e11d9d3a44d60cfd77097653a3b9617e0b53a67f61acade4f
-size 4003
+oid sha256:28514ab40c147d1b6b99a044a71fe1f211b70d589d94753a09c79cde59164c24
+size 4005
--- Assets/Scripts/Device/DoorSwitch.cs
+++ Assets/Scripts/Device/DoorSwitch.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bcd54f65636c7947892cc7e4cb5d8ba8eb6a3aca165e320593acc6f2c3092659
-size 1358
+oid sha256:20715694f92b9b0b04d90b39fd0973706261fc46aeded601f9c95f90be9f6c3b
+size 1360
