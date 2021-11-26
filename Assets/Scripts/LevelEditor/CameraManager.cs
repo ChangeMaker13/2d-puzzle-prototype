a1b14c119de45ac2683cbe3599e0c542de83e18b
Lemiraum <hope9667@gmail.com>
2021-11-27 01:38:49 +0900

    LevelEditor modify

--- Assets/Scripts/LevelEditor/CameraManager.cs
+++ Assets/Scripts/LevelEditor/CameraManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d2da8c3672ca7305d5b1d6e6e3982dbca51690f14160e8ddcf1990337421c422
-size 1904
+oid sha256:7749d580a379dca8bab7d43e1bb4386a981ef6a7b5b85b1ef3e8d284f1916c7a
+size 2806
--- Assets/Scripts/MainCamera.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:a0d39edf201eb00c9d1e6e454e7ae5e92deceea3f1650e2ceaea73955dc6a236
-size 792
