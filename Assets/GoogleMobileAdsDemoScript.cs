0558e98e0a54f2ccdcd43515ffce13fbad6e55bf
amkorousagi <hasmi5452@gmail.com>
2021-11-26 17:11:29 +0900

    feat : app-id, ad-id

--- Assets/GoogleMobileAdsDemoScript.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:696eccde3f66f6d52fddad39d65f6c9d5f7a13dca5658c44ded4cdafd9e08f02
-size 1386
---
+++ Assets/Scripts/GoogleMobileAdsDemoScript.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:66685f8af03370b4bdcab4a7ef402b0c8fcef5aa444e3b94d4c4d5890e23c719
+size 1386
