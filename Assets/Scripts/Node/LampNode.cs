033934538695611d58278c003eaf08926c022516
Lemiraum <hope9667@gmail.com>
2021-12-08 01:50:11 +0900

    Level scene modify

--- Assets/Scripts/Node/LampNode.cs
+++ Assets/Scripts/Node/LampNode.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:734995d65c6bb38e1c9ebee64100549aac1b29d1c4fc153eada1c5eedcdb41ba
-size 1716
+oid sha256:f4a764626335c569d9d5732e9cce47c9eab1db26b4accc97ed7a91217bd5e048
+size 1823
