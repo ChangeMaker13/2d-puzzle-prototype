f989d16c8651e54cad87a4bebc2398b1c94636c0
amkorousagi <hasmi5452@gmail.com>
2021-12-11 16:01:16 +0900

    feat : coin sounds, bgm volume control

--- Assets/Scripts/Objects/Coin.cs
+++ Assets/Scripts/Objects/Coin.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d4fd407de455932911716a7d06af85a99012c010b9ee94d3fb8c2446db0b61c
-size 746
+oid sha256:7aafeb3c56a4b0cdf666919320a0423ef2146086dd887d734fb410c2a277ba24
+size 1036
