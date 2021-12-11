2893231360a4fce7513b32bf114c3eb1402d5c4b
Lemiraum <hope9667@gmail.com>
2021-12-11 13:51:17 +0900

    LevelEditor update

--- Assets/Scripts/Node/Node.cs
+++ Assets/Scripts/Node/Node.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:62edc685910b06cbec9c067578fd323a91629e28f5b152f555fde8ccd7c66b26
-size 1643
+oid sha256:5444881bd5c973943dfd989741c2cd3f15e2d261e37dcd111ae2ec71353d218d
+size 1750
