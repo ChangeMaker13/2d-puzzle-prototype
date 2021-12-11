fb202d8567b6e8810024c9c53614b2ebacc0636d
Lemiraum <hope9667@gmail.com>
2021-12-11 14:02:43 +0900

    LevelEditor update

--- Assets/Scripts/Node/LampNode.cs
+++ Assets/Scripts/Node/LampNode.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a82656a00a836bd221a50238e164d0072c3b0407fedbadc1146593e729cdde11
-size 1906
+oid sha256:2807327d499e1ff9679b30a9526d6cfb7a4a2d4e326b3f285b0deeb3e33063a9
+size 1989
