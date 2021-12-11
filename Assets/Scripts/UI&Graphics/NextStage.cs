98354e063c2fd628f0ebaaf764a8648dcdc98b36
amkorousagi <hasmi5452@gmail.com>
2021-12-11 13:47:18 +0900

    refac : select world 신에서 stage관련 작은 실수 수정, sample scene에서 stage를 이용해 load current game scene, load next game scene 등 수정

--- Assets/Scripts/UI&Graphics/NextStage.cs
+++ Assets/Scripts/UI&Graphics/NextStage.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5f814f29e480f8b7721f1025aa0d06dc9f7070eed62af44429488f248c7a1938
-size 808
+oid sha256:15076b48e64674126bd1b4740c1bad34763e713233bd255f6ebd494d1adc9542
+size 807
