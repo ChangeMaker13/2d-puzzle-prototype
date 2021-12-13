50d2764ca335a1e458eba6f7a8b411960a7b1330
ChangeMaker13 <wns7756@naver.com>
2021-12-13 19:24:09 +0900

    lay coins(1~20) & bike reset bug fix

--- Assets/Scripts/Player/Bike.cs
+++ Assets/Scripts/Player/Bike.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1709814721e5e6f0d71f54812be69d20abfb36372424cb3a27d430f1a4b43da8
-size 2976
+oid sha256:c8b6f3d1a69fbeb7019f65a94260bb84ec554bb793e808c6a145caf1992a1060
+size 3477
