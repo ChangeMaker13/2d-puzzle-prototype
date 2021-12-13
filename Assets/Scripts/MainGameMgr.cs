bd51a470c325a6371fe6e70145300324799e8b5b
amkorousagi <hasmi5452@gmail.com>
2021-12-13 22:29:24 +0900

    feat : 월드선택신 스테이지 별 및 잠금기능

--- Assets/Scripts/MainGameMgr.cs
+++ Assets/Scripts/MainGameMgr.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7794c918285dd96e21d15ac0288f61e562b646bcf46841072c94bb5eb0061dd3
-size 3112
+oid sha256:a102804bf4d81f1b28ae06982adf8423bc100eaa6faf9c64f005b1ec219e5c5d
+size 3157
