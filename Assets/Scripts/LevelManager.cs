0edf28370bd3460976f69228925f5480b6d70833
Lemiraum <hope9667@gmail.com>
2021-12-14 21:18:31 +0900

    LevelEditor update

--- Assets/Scripts/LevelManager.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:ff8a14c120c1dbf927cd94b539fdd2e3609de4a3755abad036d6f41602ce9930
-size 1725
---
+++ Assets/Scripts/LevelSystem.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ac07896c880038ced41a34a6ed69ab49df40ff6d593a8a4147b33d25a54e45e1
+size 700
