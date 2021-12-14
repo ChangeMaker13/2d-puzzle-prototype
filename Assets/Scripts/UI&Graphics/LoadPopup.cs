cc6e2f275484af42f28c68ea5ccd8d5f4026a4b3
amkorousagi <hasmi5452@gmail.com>
2021-12-15 05:20:15 +0900

    feat : 맵 서버 로드 ui 및 스크립트 추가

--- Assets/Scripts/UI&Graphics/LoadPopup.cs
+++ Assets/Scripts/UI&Graphics/LoadPopup.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9cb7445ec59eb01ded829453662943fd243056105a9d35924cc8ca2b86267af2
-size 314
+oid sha256:d9a7557fd8e55dcf571b2bb65a0e1786f526d811afcf2bb0828c2d5c4f4fa104
+size 1701
