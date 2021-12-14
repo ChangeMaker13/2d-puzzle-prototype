649286849384f59f7a88eaee64618b2cfaccc2d4
amkorousagi <hasmi5452@gmail.com>
2021-12-15 02:07:39 +0900

    refac : LevelEditor에서 ui 에셋 교체

--- Assets/Scripts/Sounds/audioController.cs
+++ Assets/Scripts/Sounds/audioController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4b23e500000fd287f7708c4fee09d64a8656127d620190d6a5002a2e04dbd364
+oid sha256:aeeec8fa62e7ef9c6404ece70fa58158b4d0279aa766a272586e2e62421ed643
 size 622
