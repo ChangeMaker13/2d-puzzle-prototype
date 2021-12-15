bfb31719a78d4a94cdd825e984d18fe2c349ff2b
ChangeMaker13 <wns7756@naver.com>
2021-12-15 11:36:59 +0900

    mobile input temporary

--- Assets/Scripts/Player/KeyboardInput.cs
+++ Assets/Scripts/Player/KeyboardInput.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5ad1d0753c5229546422af04b80d1eca0851435643190cb4902930b9e6f6e4ed
-size 728
+oid sha256:32e8a9a6355c103dc916be829c94c132f3a708bc526426ea5658a3294c7e7e7e
+size 636
---
+++ Assets/Scripts/Player/MobileInput.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8053b990122e9e90d2a3ba6f0b3da1281da481cba877a02ef0ae78fb6cbeb0dc
+size 1771
