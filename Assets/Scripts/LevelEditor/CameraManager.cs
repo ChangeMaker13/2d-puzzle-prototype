de7f9e6a12bfd6c23ccdedd3a0e7c09c9a5c4a4c
ChangeMaker13 <wns7756@naver.com>
2021-12-15 20:04:16 +0900

    mobile input

--- Assets/Scripts/LevelEditor/CameraManager.cs
+++ Assets/Scripts/LevelEditor/CameraManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b1aab5895db8aac1256cd0dc2cc884d984ea2a1640110b305a81ecb3f9e321b9
-size 3536
+oid sha256:37a0ce66f241fbce1631c628a29ad0ec59bf830ad5276dbb8ae5f13dbb798f66
+size 4517
--- Assets/Scripts/LevelEditor/ObjectDataManager.cs
+++ Assets/Scripts/LevelEditor/ObjectDataManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:90af2926d1d5974e292c9e8979cf4dbd065c80ee4020bc572d2f8554104e63ce
-size 19452
+oid sha256:5ff03ac161ff7f22dffe076de9a8e6acd68ce7b4c7ddbba3441a947e3a99dcff
+size 21228
