6abf9c82e62948440950d6c96a8ac46f4b8af55b
Lemiraum <hope9667@gmail.com>
2021-12-16 20:36:05 +0900

    LevelEditor update

--- Assets/Scripts/LevelEditor/Buttons/AccelOrParkButton.cs
+++ Assets/Scripts/LevelEditor/Buttons/AccelOrParkButton.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3511ea69f8f152aee55708b8e498fe24350df1322b8684b42e607ce561e23096
-size 1390
+oid sha256:615d98a84bdc8b8d3210efd150b3e5f5dfef4b8ce63de0f8d43001b9ab3541fd
+size 1621
