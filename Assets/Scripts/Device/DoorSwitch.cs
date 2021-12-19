60441e6ec4dc2198d73a50a19085b60907dab4f1
unknown <hope9667@gmail.com>
2021-12-19 15:00:25 +0900

    Resource modify

--- Assets/Scripts/Device/DoorSwitch.cs
+++ Assets/Scripts/Device/DoorSwitch.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:20715694f92b9b0b04d90b39fd0973706261fc46aeded601f9c95f90be9f6c3b
-size 1360
+oid sha256:7204e3b498092016457d74b697a31dcf5bbf1193dfc5655dc130c37be9a31dde
+size 1939
