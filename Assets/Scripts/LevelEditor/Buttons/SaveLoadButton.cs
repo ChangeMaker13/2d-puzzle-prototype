97247ff0a6afb20ac519a51c30246f8defd50327
amkorousagi <hasmi5452@gmail.com>
2021-12-19 12:44:01 +0900

    feat : Local Level Editor

--- Assets/Scripts/LevelEditor/Buttons/SaveLoadButton.cs
+++ Assets/Scripts/LevelEditor/Buttons/SaveLoadButton.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f1e9aee91a3b9737f8c4cacf7a3b26ecba91473d237978e36fc46a0f060e782e
-size 645
+oid sha256:d995869629435a0a6c7beda732d425e2c44bcc722f46defe413911d9569162ad
+size 655
--- Assets/Scripts/LevelEditor/ObjectDataManager.cs
+++ Assets/Scripts/LevelEditor/ObjectDataManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3da6896c12dd9d0cbed278f83da65b3f349c116420aeded0c283ad323e2c019e
-size 21833
+oid sha256:b19085cea093f6a6e816775ca3421ff1f5b61b13e1cf28683fa9546cbe6df7da
+size 23450
