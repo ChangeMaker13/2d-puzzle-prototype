12fb43cde35e6b2dda38153c8e400c06200a0e2b
ChangeMaker13 <wns7756@naver.com>
2021-12-21 21:28:53 +0900

    LocalLevelEditor

--- Assets/Scripts/LevelEditor/Buttons/SaveLoadButton.cs
+++ Assets/Scripts/LevelEditor/Buttons/SaveLoadButton.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d82b1d9130209910f29bac6ecb8352d32c5245a1955c980cae33f6aa1ba1cf91
-size 669
+oid sha256:8752421e2d6f7b10e66f420a8d532ba8afd3a1efaf574c5de51c452cfcd4c512
+size 678
